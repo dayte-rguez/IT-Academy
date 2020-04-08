eef775e28835b061f9df19792943096efea1bfee
dayte <dayte.rodriguez@gmail.com>
2020-04-08 13:27:22 +0200

    GUI created

---
+++ Prácticas/Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Suma()
+        {
+            
+        }
+
+        private void btPress7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPressAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPress0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPressResult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPressSubstract_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPressMultiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btPressDivide_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
