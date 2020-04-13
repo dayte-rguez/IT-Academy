6d6337f2e8b38bea99563659222d45e92ca08921
dayte <dayte.rodriguez@gmail.com>
2020-04-13 16:20:07 +0200

    Introduction to LINQ Queries Understanding String.Format

---
+++ Código durante las videoclases/LINQ/linqIntro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace linqIntro
+{
+    class IntroToLINQ
+    {
+        static void Main()
+        {
+            // The Three Parts of a LINQ Query:
+            // 1. Data source.
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            // 3. Query execution.
+            foreach (int num in numQuery)
+            {
+                /*String.Format method 
+                 * In this case, ",1", indicates a space of 1 character after num is written
+                 */
+                Console.Write("{0,1} ", num);
+            }
+        }
+    }
+}
