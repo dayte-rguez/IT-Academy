b137ca93a9dabd330ea8e7ddc404fa475700723a
dayte <dayte.rodriguez@gmail.com>
2020-04-15 11:44:01 +0200

    Calculates avg, min and max of the array

---
+++ Ejercicios y evaluaciones/LINQ/Fase2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Fase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create data source */
+            int[] arrayNum = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+
+            /* Create the queries */
+            var average = arrayNum.Average();
+            var min = arrayNum.Min();
+            var max = arrayNum.Max();
+
+            /* Execute the queries */
+            Console.Write("The average of the array is {0}, the minimum is {1} and the maximun {2}", average, min, max);
+        }
+    }
+}
