8cf390565cdd9d561617defc8ea206cbebcc88c0
dayte <dayte.rodriguez@gmail.com>
2020-04-15 15:46:58 +0200

    Output more clear

--- Ejercicios y evaluaciones/LINQ/Fase2/Program.cs
+++ Ejercicios y evaluaciones/LINQ/Fase2/Program.cs
@@ -10,13 +10,19 @@ namespace Fase2
             /* Create data source */
             int[] arrayNum = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
 
-            /* Create the queries */
+            /* Calculate */
             var average = arrayNum.Average();
             var min = arrayNum.Min();
             var max = arrayNum.Max();
 
-            /* Execute the queries */
-            Console.Write("The average of the array is {0}, the minimum is {1} and the maximun {2}", average, min, max);
+            /* Show */
+            Console.WriteLine("Original array of numbers: ");
+            foreach (var number in arrayNum)
+            {
+                Console.Write("{0,1} ", number);
+            }
+
+            Console.Write("\n\nThe average of the array is {0}, the minimum is {1} and the maximun {2}\n\n", average, min, max);
         }
     }
 }
