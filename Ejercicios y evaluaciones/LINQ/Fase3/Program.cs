6fd27faaca7d45bb3e8666d0e4e9df67ae6581dc
dayte <dayte.rodriguez@gmail.com>
2020-04-15 16:59:45 +0200

    In method syntax added

--- Ejercicios y evaluaciones/LINQ/Fase3/Program.cs
+++ Ejercicios y evaluaciones/LINQ/Fase3/Program.cs
@@ -10,17 +10,23 @@ namespace Fase3
             /* Create data source */
             int[] arrayNum = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
 
-            /* Create the queries */
-            var greaterThanFive = from num in arrayNum
-                            where num > 5
-                            select num;
-            var lessThanFive = from num in arrayNum
-                                  where num < 5
-                                  select num;
+            /* Create the queries and dump results to arrays*/
+            //var greaterThanFive = (from num in arrayNum
+            //                where num > 5
+            //                select num).ToArray();
+            //var lessThanFive = (from num in arrayNum
+            //                      where num < 5
+            //                      select num).ToArray();
+
+            /* In method syntax */
+            var greaterThanFive = arrayNum.Where(num => num > 5).ToArray();
+            var lessThanFive = arrayNum.Where(num => num < 5).ToArray();
+
+            /* Alternative distinct output */
             var distinctGreater = greaterThanFive.Distinct();
             var distinctLess = lessThanFive.Distinct();
 
-            /* Execute the queries and show the arrays */
+            /* Show the arrays */
             Console.WriteLine("Original array of numbers: ");
             foreach (var number in arrayNum)
             {
@@ -33,13 +39,13 @@ namespace Fase3
                 Console.Write("{0,1} ", number);
             }
 
-            Console.WriteLine("\n\nArray of distinct numbers > 5: ");
+            Console.WriteLine("\n\nAlternative output: Array of distinct numbers > 5: ");
             foreach (var number in distinctGreater)
             {
                 Console.Write("{0,1} ", number);
             }
 
-            Console.WriteLine("\n\nArray of numbers < 5: ");
+            Console.WriteLine("\n\nAlternative output: Array of numbers < 5: ");
             foreach (var number in lessThanFive)
             {
                 Console.Write("{0,1} ", number);
