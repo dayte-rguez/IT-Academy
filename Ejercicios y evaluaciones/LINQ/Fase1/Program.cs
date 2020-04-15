399021286870e697277bafb3634aab761b4618f4
dayte <dayte.rodriguez@gmail.com>
2020-04-15 15:39:59 +0200

    In method syntax added

--- Ejercicios y evaluaciones/LINQ/Fase1/Program.cs
+++ Ejercicios y evaluaciones/LINQ/Fase1/Program.cs
@@ -10,16 +10,26 @@ namespace Ejercicio_LINQ
             /* Create data source */
             int[] arrayNum = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
 
-            /* Create the query */
-            var arrayEven = from num in arrayNum
-                            where (num % 2) == 0
-                            select num;
+            /* Create the query and dump results to an array */
+            //var arrayEven = (from num in arrayNum
+            //                where (num % 2) == 0
+            //                select num).ToArray();
 
-            /* Execute the query */
-            foreach (var evenNum in arrayEven)
+            /* In method syntax */
+            var arrayEven = arrayNum.Where(num => num % 2 == 0).ToArray();
+
+            /* Show the arrays */
+            Console.WriteLine("Original array of numbers: ");
+            foreach (var number in arrayNum)
+            {
+                Console.Write("{0,1} ", number);
+            }
+
+            Console.WriteLine("\n\nArray of even numbers: ");
+            foreach (var num in arrayEven)
             {
                 /* Shows each number like a column separated by a space*/
-                Console.Write("{0,2}", evenNum);
+                Console.Write("{0,2}", num);
             }
         }
     }
---
+++ Ejercicios y evaluaciones/LINQ/Fase4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create data source */
+            string[] arrayNames = { "David", "Sergio", "Maria", "Laura", "Oscar", "Julia", "Oriol" };
+
+            /* Create the queries */
+            var oNames = from name in arrayNames
+                         where name.
+
+        }
+    }
+}
